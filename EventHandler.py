3ef70837ac7de3f003a819d5eae1fde37a38240a
lkamari <lkamari@pedago.iut-blagnac.fr>
2022-03-06 23:09:17 +0100

    EventHandler modif

--- EventHandler.py
+++ EventHandler.py
@@ -2,3 +2,57 @@ import shutil
 from datetime import date
 from pathlib import Path
 from watchdog.events import FileSystemEventHandler
+from automation_script.extensions import extension_paths
+
+
+def add_date_to_path(path: Path):
+    
+    dated_path = path / f'{date.today().year}' / f'{date.today().month}' / f'{date.today().day}'
+    dated_path.mkdir(parents = True, exist_ok=True)
+    return dated_path 
+
+def rename_file(source: Path, destination_path: Path):
+    """
+        Helper function that renames file to reflect new path. If a file of the same
+        name already exists in the destination folder, the file name is numbered and
+        incremented until the filename is unique (prevents overwriting files).
+    
+    :param Path source: 
+        source of file to be moved
+    :param Path destination_path: 
+        path to destination directory
+    """
+    
+    if Path(destination_path / source.name).exists():
+        increment = 0
+        
+        while True:
+            increment += 1
+            new_name = destination_path / f'{source.stem}_{increment}{source.suffix}'
+            
+            if not new_name.exists():
+                return new_name
+            
+        else:
+            return destination_path / source.name
+        
+class EventHandler(FileSystemEventHandler):
+    
+    def __init__(self, watch_patch: Path, destination_root: Path):
+        self.watch_patch = watch_patch.resolve()
+        self.destination_root = destination_root.resolve()
+        
+    def on_modified(self, event):
+        """Called when a file or directory is modified.
+
+        :param event:
+            Event representing file/directory modification.
+        :type event:
+            :class:`DirModifiedEvent` or :class:`FileModifiedEvent`
+        """
+        for child in self.watch_patch.iterdir():
+            if child.is_file() and child.suffix.lower() in extension_paths:
+                destination_path = self.destination_root / extension_paths[child.suffix.lower()]
+                destination_path = add_date_to_path(path=destination_path)
+                destination_path = rename_file(source=child, destination_path=destination_path)
+                shutil.move(src=child, dst=destination_path)
